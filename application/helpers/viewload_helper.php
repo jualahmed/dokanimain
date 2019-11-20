e4a04c11cb2a8ed8e2a4503b57ea9d6dd52063b5
jual ahmed <md.jual.ah@gmail.com>
2019-11-20 09:06:44 +0600

    sdf

--- application/helpers/viewload_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function __viewrender($viewname='',$data='')
-{
-	$this->load->view('include/header');
-	$this->load->view($viewname,$data);
-	$this->load->view('include/footer');
-}
--- assets/js/vuejs/vuejscomp/financial_report_form_view.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-		$("#reset_btn").click(function(event) {
-		event.preventDefault();
-				$('#datep').val('');
-				$('#datepp').val('');
-				$('#datep2').val('');
-		});
-	});
